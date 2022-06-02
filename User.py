4e346b222cc6b315ff8ad8bfd7df26beb55c3a29
Evan Garcia <evanag116@yahoo.com>
2022-06-02 09:10:43 -0500

    exercise completed

--- User.py
+++ User.py
@@ -1,1 +1,29 @@
-# your improved User class goes here
+from datetime import datetime
+
+class User:
+    all_posts = {}
+    def __init__(self, name, email_address, phone_number, age):
+        self.name = name
+        self.email_address = email_address
+        self.phone_number = str(phone_number)
+        self.age = age
+
+    def post(self, str):
+        time = datetime.now()
+        self.all_posts[time] = str
+
+    def get_all_posts(self):
+        return self.all_posts
+    
+    
+
+
+# nathan = User("Nathan", "nathan@codeplatoon.org", 1231231230)
+
+evan = User("Evan", "evan@codeplatoon.org", 5027152210, 23)
+
+evan.post("my first post")
+evan.post("my second post")
+evan.post("my third post")
+
+print(evan.get_all_posts())
