d1b7f41b2ff6fc76ed495fc24b6f710c44105272
Evan Garcia <evanag116@yahoo.com>
2022-06-05 23:30:36 -0500

    exercise completed

--- User.py
+++ User.py
@@ -9,21 +9,28 @@ class User:
         self.age = age
 
     def post(self, str):
-        time = datetime.now()
-        self.all_posts[time] = str
+        time = (datetime.now()).strftime("%H:%M:%S")
+        self.all_posts[str] = time
+
+    def delete(self, str):
+        self.all_posts.pop(str)
 
     def get_all_posts(self):
         return self.all_posts
+
+
     
     
 
 
-# nathan = User("Nathan", "nathan@codeplatoon.org", 1231231230)
+nathan = User("Nathan", "nathan@codeplatoon.org", 1231231230)
+
 
 evan = User("Evan", "evan@codeplatoon.org", 5027152210, 23)
 
 evan.post("my first post")
 evan.post("my second post")
 evan.post("my third post")
+evan.delete("my second post")
 
 print(evan.get_all_posts())
